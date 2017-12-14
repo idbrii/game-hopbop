2071dd4d345858552ab6f661d0e216823d946da6
David Briscoe <idbrii@gmail.com>
2017-12-14 08:26:03 -0800

    SCARY: Switching from built-in physics to my fuzzx (I think)
    I think I built my own "physics" system called Fuzzx and wrote a player
    controller to work with it. Not sure if it works, but I didn't want to
    leave these uncommitted for too long lest I build on top of them.

---
+++ Assets/Scripts/FuzzxPlayerControl.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+// Player control for object using my custom fuzzx.
+public class FuzzxPlayerControl : MonoBehaviour {
+
+    private PlatformerBody2D body;
+
+    // Does nothing. Only exists to visualize current speed.
+    public float DEBUG_speedDisplay = 0.0f;
+
+    // Use this for initialization
+    void Start() {
+        body = GetComponent<PlatformerBody2D>();
+    }
+
+    public enum JumpRequest {
+        Idle,
+        Jump,
+        Stop,
+    };
+
+    public enum AirState {
+        OnGround,
+        Rising,         // TODO: Add more force the longer you hold.
+        // Neutral,
+        Falling,
+    };
+
+    // public to visualize values during debugging
+    public JumpRequest jump = JumpRequest.Idle;
+    public AirState air = AirState.OnGround;
+    public float moveDirection = 0.0f;
+
+    public float maxSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update() {
+
+        switch (jump) {
+            case JumpRequest.Idle:
+                if (IsRequestingJump()) {
+                    jump = JumpRequest.Jump;
+                }
+                break;
+
+            case JumpRequest.Jump:
+                if (false == IsRequestingJump()) {
+                    jump = JumpRequest.Stop;
+                }
+                break;
+
+            case JumpRequest.Stop:
+                // wait here until we are allowed to jump again.
+                if (air == AirState.OnGround) {
+                    jump = JumpRequest.Idle;
+                }
+                break;
+        } 
+
+
+        moveDirection = GetRequestedMove();
+
+    }
+
+    // FixedUpdate is called once every x seconds (multiple times per frame)
+    void FixedUpdate() {
+        if (air == AirState.Rising) {
+            if (jump == JumpRequest.Stop) {
+                // TODO: Only start falling when moving in direction of gravity?
+                //air = AirState.Neutral;
+                air = AirState.Falling;
+            }
+        }
+        else {
+            if (jump == JumpRequest.Jump) {
+                StartJump();
+                air = AirState.Rising;
+            }
+        }
+
+        if (moveDirection == 0.0f) {
+        }
+        else {
+            float velocity = body.velocity.x;
+            DEBUG_speedDisplay = velocity;
+
+            float negativeIfChangingDirections = Math.Sign(velocity) * Math.Sign(moveDirection);
+
+            StartMove(moveDirection, Math.Abs(velocity) / maxSpeed * negativeIfChangingDirections);
+        }
+
+    }
+
+    void StartJump() {
+        body.AccelerateUp(1.0f);
+        Debug.Log("started jump", this);
+    }
+
+    void StartMove(float move_input, float speedLimiterProgress) {
+        body.AccelerateRight(Math.Sign(move_input));
+        /*~ 
+        Debug.Log("started move", this);
+        */
+    }
+
+    bool IsRequestingJump() {
+        return CrossPlatformInputManager.GetButtonDown("Jump")
+            || CrossPlatformInputManager.GetAxis("Vertical") > 0;
+    }
+
+    float GetRequestedMove() {
+        float epsilon = 0.1f;
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        if (Math.Abs(h) > epsilon) {
+            return h;
+        }
+        else {
+            return 0.0f;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D bam) {
+        CheckForGroundTouch(bam);
+    }
+    void OnCollisionStay2D(Collision2D bam) {
+        CheckForGroundTouch(bam);
+    }
+
+    void CheckForGroundTouch(Collision2D potentialFloor) {
+        // Checking contact points ensures only jumping off supporting
+        // (gravity-interfering) surfaces.
+        Vector2 avg_normal = new Vector2(0,0);
+
+        /*~ 
+          // TODO: make contacts a member variable to prevent alloc
+        ContactPoint2D[] contacts = new ContactPoint2D[8];
+        int count = potentialFloor.GetContacts(contacts);
+        */
+        ContactPoint2D[] contacts = potentialFloor.contacts;
+        int count = contacts.Length;
+        for (int i = 0; i < count; ++i) {
+            var c = contacts[i];
+            avg_normal += c.normal;
+        }
+        avg_normal /= count;
+
+
+        float dot = Vector2.Dot(Physics.gravity, avg_normal);
+        bool isOnGround = dot < 0;
+        if (isOnGround) {
+            air = AirState.OnGround;
+        }
+        else if (air != AirState.Rising) {
+            // Probably walked off a ledge.
+            air = AirState.Falling;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlatformerBody2D.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+// A manually-controlled rigidbody for platformers.
+public class PlatformerBody2D : MonoBehaviour {
+
+    public Vector2 maxSpeed = new Vector2(5,5);
+    public Vector2 timeToMaxSpeed = new Vector2(1,2);
+    public float gravityScale = 1;
+    public Vector2 dragAccelerationMagnitude = new Vector2(1,1);
+
+    // Public for debugging purposes!
+    public Vector2 velocity = new Vector2(0,0);
+    public Vector2 maxAccelerationMagnitude;
+    public Vector2 queuedAcceleration = new Vector2(0,0);
+
+    // Use this for initialization
+    void Start() {
+        // todo: calculate based on timeToMaxSpeed.
+        maxAccelerationMagnitude = new Vector2(5,5);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    // FixedUpdate is called once every x seconds (multiple times per frame)
+    void FixedUpdate() {
+        // Only account for gravity here.
+        Vector2 gravity = Physics.gravity * gravityScale;
+        queuedAcceleration += gravity;
+
+        // v = a * t
+        velocity += queuedAcceleration * Time.deltaTime;
+        // d = v * t + .5 * a * t^2
+        Vector2 delta = velocity * Time.deltaTime + queuedAcceleration * Mathf.Pow(Time.deltaTime, 2.0f);
+
+        //~ transform.Translate(delta);
+        Vector2 pos = transform.position;
+        Vector2 target = pos;
+        target += delta;
+        GetComponent<Rigidbody2D>().MovePosition(target);
+
+        // todo: figure out actual velocity based on how we moved.
+        Vector2 new_pos = transform.position;
+        delta = new_pos - pos;
+
+        queuedAcceleration = dragAccelerationMagnitude;
+        queuedAcceleration.x *= ApplyVelocityBasedDrag(velocity.x, dragAccelerationMagnitude.x);
+        queuedAcceleration.y *= ApplyVelocityBasedDrag(velocity.y, dragAccelerationMagnitude.y);
+    }
+
+    float ApplyVelocityBasedDrag(float velocity, float drag) {
+        if (Mathf.Approximately(velocity, 0.0f)) {
+            return 0.0f;
+        }
+        else {
+            return -1.0f * Mathf.Sign(velocity);
+        }
+    }
+
+    public void AccelerateUp(float direction) {
+        queuedAcceleration.y = Mathf.Abs(direction) * maxAccelerationMagnitude.y;
+    }
+
+    public void AccelerateRight(float direction) {
+        queuedAcceleration.x = Mathf.Abs(direction) * maxAccelerationMagnitude.x;
+    }
+
+}
